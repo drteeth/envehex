d887db8357c5484856a50ac0b78e58fa53a7d979
Benjamin Moss <drteeth@gmail.com>
2015-09-19 23:01:13 -0400

    add parsing

--- lib/environment_canada_ex.ex
+++ lib/environment_canada_ex.ex
@@ -1,2 +1,51 @@
 defmodule EnvironmentCanada do
+  import SweetXml
+
+  defmodule Feed do
+    defstruct(
+      id: nil,
+      title: nil,
+      entries: []
+    )
+  end
+
+  defmodule Entry do
+    defstruct(
+      id: nil,
+      title: nil,
+      link: nil,
+      updated: nil,
+      published: nil,
+      category: nil,
+      summary: nil
+    )
+  end
+
+  def fetch(code) do
+    HTTPoison.start
+    response = HTTPoison.get!("http://weather.gc.ca/rss/city/#{code}.xml")
+
+    # TODO check response.status_code
+    body = response.body
+
+    entries = body |> xpath(~x"//entry"l) |> Enum.map fn e ->
+      %Entry{
+        title:      e |> xpath(~x"title/text()"),
+        id:         e |> xpath(~x"id/text()"),
+        title:      e |> xpath(~x"title/text()"),
+        link:       e |> xpath(~x"link/text()"),
+        updated:    e |> xpath(~x"updated/text()"),
+        published:  e |> xpath(~x"published/text()"),
+        category:   e |> xpath(~x"category/@term"),
+        summary:    e |> xpath(~x"summary/text()")
+      }
+    end
+
+    %Feed{
+      id: body |> xpath(~x"/feed/id/text()"),
+      title: body |> xpath(~x"/feed/title/text()"),
+      entries: entries
+    }
+  end
+
 end
--- test/environment_canada_ex_test.exs
+++ test/environment_canada_ex_test.exs
@@ -2,6 +2,6 @@ defmodule EnvironmentCanadaTest do
   use ExUnit.Case
 
   test "the truth" do
-    assert 1 + 1 == 2
+    IO.inspect EnvironmentCanada.fetch('on-143_e')
   end
 end
